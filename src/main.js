506588fb60acb40a274008181783800fabd6dff4
mitena <mitenachian@gmail.com>
2019-07-31 17:54:33 +0800

    +

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(ElementUI)
+
 
 Vue.config.productionTip = false
 
